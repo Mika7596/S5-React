09e0fac21294d6606b93e94813f996373dd31d88
Mika7596 <mika7596@gmail.com>
2025-04-11 15:25:00 +0200

    Created a component for the logo pic.

--- src/App.jsx
+++ src/App.jsx
@@ -2,12 +2,14 @@ import { useState } from 'react'
 import './App.css'
 import Navbar from './components/atoms/navbar/Navbar'
 import Button from './components/atoms/button/Button'
+import Logo from './components/atoms/logo/Logo'
 
 function App() {
   return (
     <>
     <Button button_content = "hola"> </Button>
     <Navbar></Navbar>
+    <Logo></Logo>
     </>
   )
 }
---
+++ src/components/atoms/logo/Logo.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import image from '../../../assets/images/logo-bookmark.svg'
+
+function Logo() {
+  return (
+    <img src={image} alt="Bookmark Logo" />
+  )
+}
+
+export default Logo
