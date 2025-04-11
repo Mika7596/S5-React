91f535cb4db64414a803f050d277d60a42cf3da0
Mika7596 <mika7596@gmail.com>
2025-04-11 14:13:48 +0200

    Added "Navbar" component and its styles.

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,13 @@
 import { useState } from 'react'
 import './App.css'
+import Navbar from './components/atoms/navbar/Navbar'
+import Button from './components/atoms/button/Button'
 
 function App() {
   return (
     <>
+    <Button button_content = "hola"> </Button>
+    <Navbar></Navbar>
     </>
   )
 }
---
+++ src/components/atoms/navbar/Navbar.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+import './navbar.css'
+
+function Navbar() {
+  return (
+    <>
+    <div className="navbar">
+        <a href="">FEATURES</a>
+        <a href="">PRICING</a>
+        <a href="">CONTACT</a>  
+    </div>
+    </>
+  )
+}
+
+export default Navbar
