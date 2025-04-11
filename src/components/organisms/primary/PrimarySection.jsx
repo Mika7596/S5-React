91286a388ccf84d0d42dd5bef42e3e317e6acef8
Mika7596 <mika7596@gmail.com>
2025-04-11 23:26:56 +0200

    Added styles to the first section

--- src/components/organisms/primary/PrimarySection.jsx
+++ src/components/organisms/primary/PrimarySection.jsx
@@ -1,6 +1,7 @@
 import React from 'react'
 import './primary-section.css'
 import image from '../../../assets/images/illustration-hero.svg'
+import Button from '../../atoms/button/Button'
 
 function PrimarySection() {
   return (
@@ -8,8 +9,10 @@ function PrimarySection() {
         <div className='content'>
             <h1>A Simple Bookmark Manager</h1>
             <p>A clean and simple interface to organize your favourite websites. Open a new browser tab and see your sites load instantly. Try it free.</p>
+            <Button button_class='button-blue' button_content='Get it on Chrome'></Button>
+            <Button button_content='Get it on Firefox'></Button>
         </div>
-        <div>
+        <div className='img'>
             <img src={image} alt="Hero illutration" />
         </div>
     </div>
