0ddbd8d7e50f7a246c94aa28674ab816455db487
Mika7596 <mika7596@gmail.com>
2025-04-13 21:39:28 +0200

    Created variables in Features.jsx to return them instead so that the return part will be more legible.

--- src/components/organisms/features/Features.jsx
+++ src/components/organisms/features/Features.jsx
@@ -4,39 +4,46 @@ import data from '../../../assets/data/features.json'
 import Button from '../../atoms/button/Button'
 
 function Features() {
+    const intro = <div className='intro'>
+                    <h2>Features</h2>
+                    <p>Our aim is to make it quick and easy for you to access your favourite websites. Your bookmarks sync between your devices so you can access them on the go.</p>
+                </div>
+
+    const selectTab = (i) =>{
+        alert(i)
+    }
+
+    const indicators = data.map((tab, i) =>(
+                        <div className='indicator-container' key={i}>
+                        <div value={i} onClick={()=>{selectTab(i)}}>{tab.indicator}</div>
+                        </div>
+                        ))
+
+    const tabs = data.map((tab, i) =>(
+                    <div className='tab' id={i} key={i}>
+                        <div className='img'>
+                            <img src={tab.img} alt="" />
+                        </div>
+                        <div className='tab-content'>
+                            <h3>{tab.title}</h3>
+                            <p>{tab.paragraph}</p>
+                            <Button button_class = "button-blue" button_content={tab.button}></Button>
+                        </div>
+                    </div>  
+                    ));
 
   return (
     <>
-        <div className='intro'>
-            <h2>Features</h2>
-            <p>Our aim is to make it quick and easy for you to access your favourite websites. Your bookmarks sync between your devices so you can access them on the go.</p>
-        </div>
+       {intro}
 
         <div className='indicators'>
-            {
-                data.map((tab, i) =>(
-                    <div className='indicator-container' key={i}>
-                        <div id={i}>{tab.indicator}</div>
-                    </div>
-                ))
-            }
+            {indicators}
         </div>
 
         <div className='slider-container'>
-            {
-                data.map((tab, i) =>(
-                  <div className='tab' id={i} key={i}>
-                    <div className='img'>
-                        <img src={tab.img} alt="" />
-                    </div>
-                    <div className='tab-content'>
-                        <h3>{tab.title}</h3>
-                        <p>{tab.paragraph}</p>
-                        <Button button_class = "button-blue" button_content="More info"></Button>
-                    </div>
-                  </div>  
-                ))
-            }
+            <div className='slider'>
+                {tabs}
+            </div>
         </div>
     </>
   )
