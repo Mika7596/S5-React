6be5bce5ec37fbca201da4b2899167bb7f3e06ae
Mika7596 <mika7596@gmail.com>
2025-04-14 16:58:29 +0200

    Created the details menu and its styles.

--- src/components/organisms/FAQs/FAQs.jsx
+++ src/components/organisms/FAQs/FAQs.jsx
@@ -1,25 +1,36 @@
 import React from 'react'
 import styles from './faqs.module.css'
 import Button from '../../atoms/button/Button'
+import { useState } from 'react'
 
 function FAQs() {
+    
     const intro = <div className={styles.intro}><h2>Frequently Asked Questions</h2>
                 <p>Here are some of our FAQs. If you have any other questions you'd like answered please feel free to email us.</p></div>
 
-    const details = <div><details>
-        <summary>What is Bookmark</summary>
-        <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Ipsa, totam et accusantium rerum velit, ab perferendis neque veniam, molestias recusandae expedita quasi soluta ut! Ex aliquam possimus quia vitae repellendus.</p></details>
-        <details>
-        <summary>How can I request a new browser?</summary>
-        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Vitae similique quidem dolor nulla ipsa, aut unde, ad quis magnam quia, voluptate dolore debitis commodi velit sint! Corporis sint architecto maiores?</p>
+    const questions = ["What is Bookmark?", "How can I request a new browser?", "Is there a mobile app?", "What about other Chromium browsers?"]
 
-    </details></div>
+     
+    
+    
+    const details = questions.map((question, i) =>(
+            <div key={i}>
+                <button >{question}</button>
+                <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Quaerat, similique numquam ea veritatis delectus aliquam, veniam doloribus necessitatibus aperiam officiis eaque labore nobis incidunt quod voluptates non voluptas, iure alias!</p>
+            </div>
+        )
+
+        )
+   
+
   return (
-    <>
+    <div className={styles.faqs}>
     <div>{intro}</div>
-    {details}
-    <Button button_content="More Info" button_class="button-blue"></Button>
-    </>
+    <div className={styles.details}>{details}</div>
+
+
+    <Button button_content="More Info" button_class={"button-blue"}></Button>
+    </div>
   )
 }
 
