cccc0ce682961f5eb45445afca88aa9e15c003a4
Mika7596 <mika7596@gmail.com>
2025-04-16 10:37:15 +0200

    Created lower footer's content.

--- src/components/molecules/lower-footer/LowerFooter.jsx
+++ src/components/molecules/lower-footer/LowerFooter.jsx
@@ -1,8 +1,20 @@
 import React from 'react'
+import styles from './lowerFooter.module.css'
+import Logo from '../../atoms/logo/Logo'
+import Navbar from '../../atoms/navbar/Navbar'
+import facebook from '../../../assets/images/icon-facebook.svg'
+import twitter from '../../../assets/images/icon-twitter.svg'
 
 function LowerFooter() {
   return (
-    <div>LowerFooter</div>
+    <div className={styles.container}>
+      <Logo></Logo>
+      <div><Navbar></Navbar></div>
+      <div className={styles.media}>
+        <img src={facebook} alt="facebook icon" />
+        <img src={twitter} alt="Twitter icon" />
+      </div>
+    </div>
   )
 }
 
